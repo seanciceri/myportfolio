140a9bc3bf2a04413640481b3f58e1f789f42740
sean ciceri <seannolo2401@gmail.com>
2025-02-07 00:29:19 +0100

    Inizio sviluppo carosello iniziale

--- scripts.js
+++ scripts.js
@@ -2,3 +2,19 @@
 function toggleMenu() {
     document.querySelector('.navbar-nav').classList.toggle('show');
 }
+
+//INIZIALIZZAZIONE SLICK Carousel
+$(document).ready(function(){
+    $('.carousel').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,  // Disabilita i puntini
+        arrows: false, // Attiva le frecce
+        prevArrow: '<button class="slick-prev">&#10094;</button>',
+        nextArrow: '<button class="slick-next">&#10095;</button>'
+    });
+  });
+      
+
